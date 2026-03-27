4ca52b3fc11d999a0b55e1bd9714dfdb1843e001
nastassja90 <nas.bellisario@gmail.com>
2026-03-27 16:35:47 +0100

    feat: add cron

---
+++ supabase/migrations/20260327153517_fix_telegram_cron.sql
@@ -0,0 +1,14 @@
+SELECT cron.unschedule('telegram-notifier-daily');
+
+SELECT cron.schedule(
+  'telegram-notifier-daily',
+  '* * * * *',
+  $$
+  SELECT net.http_post(
+    'https://zlsmzlingdehpgglxpmk.supabase.co/functions/v1/telegram-notifier',
+    '{}'::jsonb,
+    '{}'::jsonb,
+    '{"apikey":"GhmRvI7Y3ciHcmEYDfXqLoUOE4JYZQwdxdVrXXhD9j8=","Content-Type":"application/json"}'::jsonb
+  ) AS request_id;
+  $$
+);
