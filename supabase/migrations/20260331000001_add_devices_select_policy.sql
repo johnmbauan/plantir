a52fb1caaf3dcc3b3028fe1688c8fc4009502dd9
John <john.m.bauan@gmail.com>
2026-04-18 16:48:38 +0200

    fix: delete policy
    Disable RLS, since we need the anonymous role (used in
    the humidity sensors) to be able to write to tables

--- supabase/migrations/20260331000001_add_devices_select_policy.sql
+++
@@ -1,3 +0,0 @@
--- Allow anon role to read devices (required for fetchDevices and INSERT...RETURNING)
-CREATE POLICY "public can read devices"
-  ON "public"."devices" FOR SELECT TO "anon" USING (true);
